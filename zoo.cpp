5ac066cdc8b88fc286369b6f59485dbbe608107f
Wei Xiang Zheng <zhengwx06@gmail.com>
2025-08-28 14:28:10 -0400

    Updated zoo.cpp to have multiple animals

--- zoo.cpp
+++ zoo.cpp
@@ -4,7 +4,18 @@
 #include "AnimalsInZoo.h"
 
 int main() {
-	Animal animal("African Elephant", 1758);
+	Animal *animal1 = new Animal("African Elephant", 1758);
+	Animal animal2("Giant Panda", 1869);
+
+   	delete animal1;
+  	animal1 = new Animal("Snow Leopard", 1777);
+
+   	animal2.display();
+   	animal1->display();
+
+   	delete animal1;
+
+	Animal animal("Giraffe", 1758);
    	AnimalsInZoo zooAnimal(animal);
 	zooAnimal.display();
 }
